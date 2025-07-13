4b3ef062fcc8f879427eeac872f8ab23b57934cc
devsafix <devsafix@gmail.com>
2025-07-14 00:01:50 +0600

    Reset function added

--- src/app/middlewares/checkAuth.ts
+++ src/app/middlewares/checkAuth.ts
@@ -3,21 +3,42 @@ import { JwtPayload } from "jsonwebtoken";
 import AppError from "../errorHelpers/AppError";
 import { verifyToken } from "../utils/jwt";
 import { envVariables } from "../config/env";
+import { User } from "../modules/user/user.model";
+import httpStatus from "http-status-codes";
+import { IsActive } from "../modules/user/user.interface";
 
 export const checkAuth =
   (...authRoles: string[]) =>
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const accessToken = req.headers.authorization;
-
       if (!accessToken) {
-        throw new AppError(403, "No Token Received");
+        throw new AppError(403, "No Token Recieved");
       }
 
       const verifiedToken = verifyToken(
         accessToken,
         envVariables.JWT_ACCESS_SECRET
       ) as JwtPayload;
+
+      const isUserExist = await User.findOne({ email: verifiedToken.email });
+
+      if (!isUserExist) {
+        throw new AppError(httpStatus.BAD_REQUEST, "User does not exist");
+      }
+      if (
+        isUserExist.isActive === IsActive.BLOCKED ||
+        isUserExist.isActive === IsActive.INACTIVE
+      ) {
+        throw new AppError(
+          httpStatus.BAD_REQUEST,
+          `User is ${isUserExist.isActive}`
+        );
+      }
+      if (isUserExist.isDeleted) {
+        throw new AppError(httpStatus.BAD_REQUEST, "User is deleted");
+      }
+
       if (!authRoles.includes(verifiedToken.role)) {
         throw new AppError(403, "You are not permitted to view this route!!!");
       }
