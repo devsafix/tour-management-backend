5bdee43c7af7fe0ac63a0554d9d747fdb05ce365
devsafix <devsafix@gmail.com>
2025-07-14 15:32:17 +0600

    passport auth added

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,25 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import { router } from "./app/routes";
-import { globalErrorHandler } from "./app/middlewares/golbalErrorHandler";
+import { globalErrorHandler } from "./app/middlewares/globalErrorHandler";
 import notFound from "./app/middlewares/notFound";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
+import passport from "passport";
+import expressSession from "express-session";
 
 const app: Application = express();
 
+app.use(
+  expressSession({
+    secret: "",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(
   cors({
     origin: ["http://localhost:5173"],
