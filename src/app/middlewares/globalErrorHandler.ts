a09909568c121bdc6add089b4735408c07a41455
devsafix <devsafix@gmail.com>
2025-07-15 22:06:41 +0600

    helper created for error handle properly

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -3,6 +3,11 @@
 import { NextFunction, Request, Response } from "express";
 import { envVariables } from "../config/env";
 import AppError from "../errorHelpers/AppError";
+import { handlerDuplicateError } from "../helpers/handleDuplicateError";
+import { handleCastError } from "../helpers/handleCastError";
+import { handlerZodError } from "../helpers/handlerZodError";
+import { handlerValidationError } from "../helpers/handlerValidationError";
+import { TErrorSources } from "../interfaces/error.types";
 
 export const globalErrorHandler = (
   err: any,
@@ -10,13 +15,48 @@ export const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  let errorSources: TErrorSources[] = [];
   let statusCode = 500;
-  let message = "Something went wrong";
+  let message = "Something Went Wrong!!";
 
-  if (err instanceof AppError) {
+  //Duplicate error
+  if (err.code === 11000) {
+    const simplifiedError = handlerDuplicateError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+  }
+
+  // Object ID error / Cast Error
+  else if (err.name === "CastError") {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+  }
+
+  // Zod error
+  else if (err.name === "ZodError") {
+    const simplifiedError = handlerZodError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorSources = simplifiedError.errorSources as TErrorSources[];
+  }
+
+  //Mongoose Validation Error
+  else if (err.name === "ValidationError") {
+    const simplifiedError = handlerValidationError(err);
+    statusCode = simplifiedError.statusCode;
+    errorSources = simplifiedError.errorSources as TErrorSources[];
+    message = simplifiedError.message;
+  }
+
+  // App error
+  else if (err instanceof AppError) {
     statusCode = err.statusCode;
     message = err.message;
-  } else if (err instanceof Error) {
+  }
+
+  // Global error
+  else if (err instanceof Error) {
     statusCode = 500;
     message = err.message;
   }
