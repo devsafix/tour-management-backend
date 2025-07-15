0bc6f2b2883e3b625cb25007a904d14444db46aa
devsafix <devsafix@gmail.com>
2025-07-15 13:31:57 +0600

    delete controller for login with credentials

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,51 +1,11 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import AppError from "../../errorHelpers/AppError";
-import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import httpStatus from "http-status-codes";
 import bcryptjs from "bcryptjs";
-import {
-  createNewAccessTokenWithRefreshToken,
-  createUserTokens,
-} from "../../utils/userTokens";
+import { createNewAccessTokenWithRefreshToken } from "../../utils/userTokens";
 import { JwtPayload } from "jsonwebtoken";
 import { envVariables } from "../../config/env";
-
-const credentialsLoginService = async (payload: Partial<IUser>) => {
-  const { email, password } = payload;
-
-  const isUserExist = await User.findOne({ email });
-
-  if (!isUserExist) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Email does not exist");
-  }
-
-  const isPasswordMatched = await bcryptjs.compare(
-    password as string,
-    isUserExist.password as string
-  );
-
-  if (!isPasswordMatched) {
-    throw new AppError(httpStatus.BAD_REQUEST, "Incorrect Password");
-  }
-
-  const jwtPayload = {
-    userId: isUserExist._id,
-    email: isUserExist.email,
-    role: isUserExist.role,
-  };
-
-  const userTokens = createUserTokens(isUserExist);
-
-  const { password: pass, ...rest } = isUserExist.toObject();
-
-  return {
-    accessToken: userTokens.accessToken,
-    refreshToken: userTokens.refreshToken,
-    user: rest,
-  };
-};
 
 const getNewAccessTokenService = async (refreshToken: string) => {
   const newAccessToken = await createNewAccessTokenWithRefreshToken(
@@ -85,7 +45,6 @@ const resetPasswordServices = async (
 };
 
 export const AuthServices = {
-  credentialsLoginService,
   getNewAccessTokenService,
   resetPasswordServices,
 };
