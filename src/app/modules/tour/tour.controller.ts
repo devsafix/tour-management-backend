2a4776a596d6c797a585b5add7658d59475c8063
devsafix <devsafix@gmail.com>
2025-07-19 01:06:12 +0600

    update tour controller and service

--- src/app/modules/tour/tour.controller.ts
+++ src/app/modules/tour/tour.controller.ts
@@ -56,8 +56,7 @@ const getAllTourTypes = catchAsync(async (req: Request, res: Response) => {
 });
 
 const createTourType = catchAsync(async (req: Request, res: Response) => {
-  const { name } = req.body;
-  const result = await TourService.createTourType(name);
+  const result = await TourService.createTourType(req.body);
   sendResponse(res, {
     statusCode: 201,
     success: true,
@@ -68,8 +67,7 @@ const createTourType = catchAsync(async (req: Request, res: Response) => {
 
 const updateTourType = catchAsync(async (req: Request, res: Response) => {
   const { id } = req.params;
-  const { name } = req.body;
-  const result = await TourService.updateTourType(id, name);
+  const result = await TourService.updateTourType(id, req.body);
   sendResponse(res, {
     statusCode: 200,
     success: true,
