dc015aea014ee11905fb291a1719382d24d56ee4
devsafix <devsafix@gmail.com>
2025-07-20 22:56:22 +0600

    payment services added

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from "express";
-import { envVars } from "../../config/env";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
-import { PaymentService } from "./payment.service";
+import { PaymentServices } from "./payment.service";
+import { envVariables } from "../../config/env";
 
 const initPayment = catchAsync(async (req: Request, res: Response) => {
   const bookingId = req.params.bookingId;
-  const result = await PaymentService.initPayment(bookingId as string);
+  const result = await PaymentServices.initPayment(bookingId as string);
   sendResponse(res, {
     statusCode: 201,
     success: true,
@@ -16,37 +16,37 @@ const initPayment = catchAsync(async (req: Request, res: Response) => {
 });
 const successPayment = catchAsync(async (req: Request, res: Response) => {
   const query = req.query;
-  const result = await PaymentService.successPayment(
+  const result = await PaymentServices.successPayment(
     query as Record<string, string>
   );
 
   if (result.success) {
     res.redirect(
-      `${envVars.SSL.SSL_SUCCESS_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`
+      `${envVariables.SSL.SSL_SUCCESS_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`
     );
   }
 });
 const failPayment = catchAsync(async (req: Request, res: Response) => {
   const query = req.query;
-  const result = await PaymentService.failPayment(
+  const result = await PaymentServices.failPayment(
     query as Record<string, string>
   );
 
   if (!result.success) {
     res.redirect(
-      `${envVars.SSL.SSL_FAIL_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`
+      `${envVariables.SSL.SSL_FAIL_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`
     );
   }
 });
 const cancelPayment = catchAsync(async (req: Request, res: Response) => {
   const query = req.query;
-  const result = await PaymentService.cancelPayment(
+  const result = await PaymentServices.cancelPayment(
     query as Record<string, string>
   );
 
   if (!result.success) {
     res.redirect(
-      `${envVars.SSL.SSL_CANCEL_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`
+      `${envVariables.SSL.SSL_CANCEL_FRONTEND_URL}?transactionId=${query.transactionId}&message=${result.message}&amount=${query.amount}&status=${query.status}`
     );
   }
 });
