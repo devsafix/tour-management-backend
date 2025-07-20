a7621657de905fd2a5650d44e4381f6f7caa34bc
devsafix <devsafix@gmail.com>
2025-07-20 22:57:17 +0600

    payment routes updated

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -1,10 +1,9 @@
 import express from "express";
 import { PaymentController } from "./payment.controller";
 
-const router = express.Router();
+export const PaymentRoutes = express.Router();
 
-router.post("/init-payment/:bookingId", PaymentController.initPayment);
-router.post("/success", PaymentController.successPayment);
-router.post("/fail", PaymentController.failPayment);
-router.post("/cancel", PaymentController.cancelPayment);
-export const PaymentRoutes = router;
+PaymentRoutes.post("/init-payment/:bookingId", PaymentController.initPayment);
+PaymentRoutes.post("/success", PaymentController.successPayment);
+PaymentRoutes.post("/fail", PaymentController.failPayment);
+PaymentRoutes.post("/cancel", PaymentController.cancelPayment);
