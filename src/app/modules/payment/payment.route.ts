568b2fbfd65ed8a35cdc94f101fda253596b6e3d
devsafix <devsafix@gmail.com>
2025-08-04 23:48:20 +0600

    payment route added for get invoice url

--- src/app/modules/payment/payment.route.ts
+++ src/app/modules/payment/payment.route.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { PaymentController } from "./payment.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../user/user.interface";
 
 export const PaymentRoutes = express.Router();
 
@@ -7,3 +9,4 @@ PaymentRoutes.post("/init-payment/:bookingId", PaymentController.initPayment);
 PaymentRoutes.post("/success", PaymentController.successPayment);
 PaymentRoutes.post("/fail", PaymentController.failPayment);
 PaymentRoutes.post("/cancel", PaymentController.cancelPayment);
+PaymentRoutes.get("/invoice/:paymentId", checkAuth(...Object.values(Role)), PaymentController.getInvoiceDownloadUrl);
