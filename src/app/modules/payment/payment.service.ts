01e9234dfcc830e8771ca1da0752782b0fd6435d
devsafix <devsafix@gmail.com>
2025-08-04 23:49:38 +0600

    payment service added for get invoice url

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -186,9 +186,24 @@ const cancelPayment = async (query: Record<string, string>) => {
   }
 };
 
+const getInvoiceDownloadUrl = async (paymentId: string) => {
+  const payment = await Payment.findById(paymentId).select("invoiceUrl");
+
+  if (!payment) {
+    throw new AppError(401, "Payment not found");
+  }
+
+  if (!payment.invoiceUrl) {
+    throw new AppError(401, "No invoice found");
+  }
+
+  return payment.invoiceUrl;
+};
+
 export const PaymentServices = {
   initPayment,
   successPayment,
   failPayment,
   cancelPayment,
+  getInvoiceDownloadUrl,
 };
