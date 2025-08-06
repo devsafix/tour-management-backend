22a49e409fee643dd227e1bdaa0cd5e913c0ba86
devsafix <devsafix@gmail.com>
2025-08-07 00:54:15 +0600

    payment validation service added

--- src/app/modules/sslCommerz/sslCommerz.service.ts
+++ src/app/modules/sslCommerz/sslCommerz.service.ts
@@ -1,8 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import httpStatus from "http-status-codes";
 import AppError from "../../errorHelpers/AppError";
 import { ISSLCommerz } from "./sslCommerz.interface";
 import { envVariables } from "../../config/env";
 import axios from "axios";
+import { Payment } from "../payment/payment.model";
 
 const sslPaymentInit = async (payload: ISSLCommerz) => {
   try {
@@ -46,12 +48,29 @@ const sslPaymentInit = async (payload: ISSLCommerz) => {
     });
 
     return response.data;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (error: any) {
     throw new AppError(httpStatus.BAD_REQUEST, error.message);
   }
 };
 
+const validatePayment = async (payload: any) => {
+  try {
+    const response = await axios({
+      method: "GET",
+      url: `${envVariables.SSL.SSL_VALIDATION_API}?val_id=${payload.val_id}&store_id=${envVariables.SSL.STORE_ID}&store_passwd=${envVariables.SSL.STORE_PASS}`,
+    });
+
+    await Payment.updateOne(
+      { transactionId: payload.tran_id },
+      { paymentGatewayData: response.data },
+      { runValidators: true }
+    );
+  } catch (error: any) {
+    throw new AppError(401, `Payment Validation Error, ${error.message}`);
+  }
+};
+
 export const SSLServices = {
   sslPaymentInit,
+  validatePayment,
 };
