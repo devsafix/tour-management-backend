49bc8fd453f6eb8ed17a7fe7a3160c0cb4c31171
devsafix <devsafix@gmail.com>
2025-08-06 23:15:35 +0600

    cors updated

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: envVariables.FRONTEND_URL,
+    credentials: true,
   })
 );
 
