2d6fb0e34844e84c68d9d5bed9f3974775d3ca26
devsafix <devsafix@gmail.com>
2025-08-07 01:28:45 +0600

    set cookie updated

--- src/app/utils/setCookie.ts
+++ src/app/utils/setCookie.ts
@@ -1,4 +1,5 @@
 import { Response } from "express";
+import { envVariables } from "../config/env";
 
 export interface AuthTokens {
   accessToken?: string;
@@ -9,14 +10,16 @@ export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
   if (tokenInfo.accessToken) {
     res.cookie("accessToken", tokenInfo.accessToken, {
       httpOnly: true,
-      secure: false,
+      secure: envVariables.NODE_ENV === "production",
+      sameSite: "none",
     });
   }
 
   if (tokenInfo.refreshToken) {
     res.cookie("refreshToken", tokenInfo.refreshToken, {
       httpOnly: true,
-      secure: false,
+      secure: envVariables.NODE_ENV === "production",
+      sameSite: "none",
     });
   }
 };
