1ab7b54cadfef30fe306613bb2a5b5449ba3f676
devsafix <devsafix@gmail.com>
2025-08-13 16:37:37 +0600

    get all tour type api updated

--- src/app/modules/tour/tour.controller.ts
+++ src/app/modules/tour/tour.controller.ts
@@ -63,8 +63,10 @@ const deleteTour = catchAsync(async (req: Request, res: Response) => {
   });
 });
 const getAllTourTypes = catchAsync(async (req: Request, res: Response) => {
-  const result = await TourService.getAllTourTypes();
-
+  const query = req.query;
+  const result = await TourService.getAllTourTypes(
+    query as Record<string, string>
+  );
   sendResponse(res, {
     statusCode: 200,
     success: true,
--- src/app/modules/tour/tour.service.ts
+++ src/app/modules/tour/tour.service.ts
@@ -1,6 +1,9 @@
 import { deleteImageFromCLoudinary } from "../../config/cloudinary.config";
 import { QueryBuilder } from "../../utils/QueryBuilder";
-import { tourSearchableFields } from "./tour.constant";
+import {
+  tourSearchableFields,
+  tourTypeSearchableFields,
+} from "./tour.constant";
 import { ITour, ITourType } from "./tour.interface";
 import { Tour, TourType } from "./tour.model";
 
@@ -99,8 +102,25 @@ const createTourType = async (payload: ITourType) => {
   return await TourType.create({ name: payload.name });
 };
 
-const getAllTourTypes = async () => {
-  return await TourType.find();
+const getAllTourTypes = async (query: Record<string, string>) => {
+  const queryBuilder = new QueryBuilder(TourType.find(), query);
+
+  const tourTypes = await queryBuilder
+    .search(tourTypeSearchableFields)
+    .filter()
+    .sort()
+    .fields()
+    .paginate();
+
+  const [data, meta] = await Promise.all([
+    tourTypes.build(),
+    queryBuilder.getMeta(),
+  ]);
+
+  return {
+    data,
+    meta,
+  };
 };
 
 const updateTourType = async (id: string, payload: ITourType) => {
